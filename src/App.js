192e8c9417b96b6ca7e32c497df2ec289c392d32
NunezAN <alex101nunez@gmail.com>
2022-11-03 20:07:45 -0500

    countdown timer is set up

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Timer from "./component/Timer";
 function App() {
   return (
     <div className="App bg-[#7F1431] text-white h-screen flex">
-      <body className="max-w-7xl m-auto text-center space-y-8">
+      <div className="max-w-7xl m-auto text-center space-y-8">
         <h1 className="text-6xl font-bold tracking-wide">Countdown To The World Cup! </h1>
         <Timer />
         <h3 className="text-black font-bold text-4xl tracking-wide">Created By: Alex Nunez</h3>
-      </body>
+      </div>
     </div>
   );
 }
--- src/component/Timer.jsx
+++ src/component/Timer.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Timer = () => {
-    return (
-        <div>
-            <h1>This is the timer!!</h1>
-        </div>
-    );
-}
+  const worldCupDate = new Date(Date.UTC("2022", "10", "20", "16", "0", "0"));
+  const [timeNow, setTimeNow] = useState(Date.now());
+  useEffect(() => {
+    const interval = setInterval(() => setTimeNow(Date.now()), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const startTimer = () => {
+    let expirationTimer = worldCupDate - timeNow;
+    if (expirationTimer <= 0) {
+      return "Game Time";
+    }
+    const timerDays = parseInt(expirationTimer / (1000 * 60 * 60 * 24));
+    const timerHours = parseInt(expirationTimer / (1000 * 60 * 60)%24);
+    const timerMinutes = parseInt((expirationTimer / (1000 * 60)) % 60);
+    const timerSeconds = parseInt((expirationTimer / 1000) % 60);
+
+    return `${timerDays < 10 ? "0" + timerDays : timerDays}d ${timerHours < 10 ? "0" + timerHours : timerHours}h ${
+      timerMinutes < 10 ? "0" + timerMinutes : timerMinutes
+    }m ${timerSeconds < 10 ? "0" + timerSeconds : timerSeconds}s`;
+  };
+  startTimer();
+  return (
+    <div>
+     <span className="text-7xl">{startTimer()}</span>
+    </div>
+  );
+};
 
 export default Timer;
